01908fbe66a11f36c266822560c086aee83b4950
Bruce Armstrong <brucearmstrongofficial@gmail.com>
2024-03-06 14:19:29 +0530

    updated the scss path

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,17 @@
 const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 
-    
+
 
 function buildStyles() {
-    return src('./ *.scss')
+    return src('*.scss')
         .pipe(sass())
         .pipe(dest('.css'))
 }
 
 
 function watchTask() {
-    watch('./ *.scss', buildStyles)
+    watch('*.scss', buildStyles)
 }
 
 
