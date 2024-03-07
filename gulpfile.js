d3486069ac69a5001ed5dc099c42b20d61b57323
Bruce Armstrong <brucearmstrongofficial@gmail.com>
2024-03-07 18:04:02 +0530

    created themes , gulp file changes

--- gulpfile.js
+++ gulpfile.js
@@ -4,22 +4,31 @@ const purgecss = require('gulp-purgecss');
 
 
 
-function buildStyles() {
+function GenerateCSS() {
     return src('sass/**/*.scss')
         .pipe(sass())
         .pipe(purgecss({ content: ['*.html'] }))
         .pipe(dest('css'))
 }
 
-
-function watchTask() {
-    watch(["antui/**/*.scss", 'sass/**/*.scss', '*.html'], buildStyles)
+function watchSASS() {
+    watch(["antui/**/*.scss", 'sass/**/*.scss', '*.html'], GenerateCSS)
 }
+
+
 
 function build() {
     return src('antui/**/*.scss')
         .pipe(dest('dist'))
 }
 
-exports.build = build;
-exports.default = series(buildStyles, watchTask);
+
+
+function buildCSS() {
+    return src('sass/**/*.scss')
+        .pipe(sass())
+        .pipe(dest('dist'))
+}
+
+exports.build = series(buildCSS, build);
+exports.default = series(GenerateCSS, watchSASS);
